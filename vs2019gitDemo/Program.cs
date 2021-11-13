1d0f3574a3608683d9f371c51889d25c7e6b54f4
Prasannakumar <ganithiprasannakumar@gmail.com>
2021-11-13 09:21:49 +0530

    Add project files.

---
+++ vs2019gitDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vs2019gitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
